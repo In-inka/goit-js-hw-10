4ea91d0e417caa23d5ced8fcb5cf358002d6692d
In-inka <shkilnai.1995@gmail.com>
2023-05-19 02:38:14 +0300

    creating a catch error

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,11 @@
+import Notiflix from 'notiflix';
+
 export function fetchCountries(name) {
   const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
   return fetch(url)
     .then(response => {
       if (!response.ok) {
+        Notiflix.Notify.warning('Oops, there is no country with that name');
         throw new Error(response.status);
       }
       return response.json();
